775544ae5765182bc6adfb1be14a7a0b6dfc380d
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-26 13:18:15 -0400

    adds Country model

---
+++ Collection-View-Lab/Collection-View-Lab/Models/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  Collection-View-Lab
+//
+//  Created by Sunni Tang on 9/26/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Codable {
+    let name: String
+    let capital: String
+    let population: Int
+}

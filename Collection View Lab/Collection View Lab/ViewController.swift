99a1894b92996c53508972a72a9ba8ab78063217
Jason Ruan <jasonruan@pursuit.org>
2019-09-26 14:11:14 -0400

    Initial commit

---
+++ Collection View Lab/Collection View Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection View Lab
+//
+//  Created by Jason Ruan on 9/26/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Collection View Lab/Collection View LabTests/Collection_View_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Collection_View_LabTests.swift
+//  Collection View LabTests
+//
+//  Created by Jason Ruan on 9/26/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import XCTest
+@testable import Collection_View_Lab
+
+class Collection_View_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

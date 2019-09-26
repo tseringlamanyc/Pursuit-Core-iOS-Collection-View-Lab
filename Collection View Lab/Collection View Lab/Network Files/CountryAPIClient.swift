6e4bcb327ae63c91d91b69f265ab4e3509dd9b6a
Jason Ruan <jasonruan@pursuit.org>
2019-09-26 15:44:52 -0400

    Adds getCountries function to CountryAPIClient

--- Collection View Lab/Collection View Lab/Network Files/CountryAPIClient.swift
+++ Collection View Lab/Collection View Lab/Network Files/CountryAPIClient.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class CountryAPIClient {
+    private init() {}
+    static let manager = CountryAPIClient()
+    
+    private let allCountriesURLString = "https://restcountries.eu/rest/v2"
+    
+    func getCountries(completionHandler: @escaping (Result<[Country], AppError>) -> () ) {
+        guard let url = URL(string: allCountriesURLString) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        NetworkHelper.manager.performDataTask(withUrl: url, andHTTPBody: nil, andMethod: .get) { (result) in
+            switch result {
+            case .failure(_):
+                completionHandler(.failure(.noDataReceived))
+            case .success(let data):
+                do {
+                    let arrCountries = try JSONDecoder().decode([Country].self, from: data)
+                    completionHandler(.success(arrCountries))
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+}

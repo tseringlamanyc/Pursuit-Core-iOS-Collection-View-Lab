01171d1da85ac4bc976ccc985678ebe0f4dd3807
Tsering Lama <tseringlama@pursuit.org>
2020-01-14 15:03:16 -0500

    coded API client

--- CollectionViewLab/Model/CountryAPI.swift
+++ CollectionViewLab/Model/CountryAPI.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct CountryAPI {
+    static func getCountry(countrySearch: String, completionHandler: @escaping (Result<[Countries], AppError>) -> ()) {
+        let endpointURL = "https://restcountries.eu/rest/v2/name/\(countrySearch)"
+        guard let url = URL(string: endpointURL) else {
+            completionHandler(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completionHandler(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let countryData = try JSONDecoder().decode([Countries].self, from: data)
+                    completionHandler(.success(countryData))
+                } catch {
+                    completionHandler(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
